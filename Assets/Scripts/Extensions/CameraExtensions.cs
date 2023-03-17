e6a7e95df03f6291656faead317da47d1648c1c7
Elizaveta Baglaeva <e.baglaeva@furylion.dev>
2023-03-17 18:21:00 +0300

    Added tilemap

--- Assets/Scripts/Extensions/CameraExtensions.cs
+++ Assets/Scripts/Extensions/CameraExtensions.cs
@@ -11,5 +11,16 @@ namespace Extensions
             var camPosition = camera.transform.position;
             return new Bounds(camPosition, new Vector3(camWidth, camHeight));
         }
+        
+        public static T GetObject<T>()
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity,  LayerMask.GetMask("UI"));
+            if (hit.collider != null)
+                if (hit.collider.gameObject.TryGetComponent(out T o))
+                    return o;
+
+            return default;
+        }
     }
 }
--- Assets/Scripts/Gameplay/GameBoard.cs
+++ Assets/Scripts/Gameplay/GameBoard.cs
@@ -1,12 +1,18 @@
-﻿using TransformExtensions;
+﻿using System.Collections.Generic;
+using TransformExtensions;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Gameplay
 {
     public class GameBoard : MonoBehaviour
     {
         [SerializeField] private RectTransform _rectTransform;
-        
+        [SerializeField] private Tilemap _tilemap;
+        [SerializeField] private BlockObject _blockObject;
+
+        private readonly List<Vector3> _map = new();
+
         private void Awake()
         {
             DragDropMechanics.Dropped += OnObjectDropped;
@@ -14,10 +20,23 @@ namespace Gameplay
 
         private void OnObjectDropped(Collider2D obj, Vector3 defaultPos)
         {
-            if (!obj.bounds.IsInBounds(_rectTransform.Bounds()))
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var cell = _tilemap.WorldToCell(pos);
+            var newPos = _tilemap.CellToWorld(cell) + _tilemap.cellSize / 2;
+            
+            if (!obj.bounds.IsInBounds(_rectTransform.Bounds()) || _map.Contains(newPos))
                 obj.transform.position = defaultPos;
             else
+            {
                 obj.enabled = false;
+
+                var block = obj.gameObject.GetComponent<BlockObject>();
+                block.transform.SetPositionXY(newPos.x, newPos.y);
+                _map.Add(newPos);
+
+                var newBlock = Instantiate(_blockObject, transform);
+                newBlock.transform.SetPositionXY(0, -25);
+            }
         }
     }
 }
